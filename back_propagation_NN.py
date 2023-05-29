4c0ac5faddd20eab8fc2ba13d8fe35cd780c0536
Ohad Wolfman <98156296+ohadwolfman@users.noreply.github.com>
2023-05-29 15:29:04 +0300

    Add files via upload

--- back_propagation_NN.py
+++ back_propagation_NN.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 print(" -----loading the files------- ")
 # from the folder 'קבצים (מ,ב,ל)' we generated united file:
 file1 = 'Data_regular_writing.txt'
@@ -49,7 +50,7 @@ def cleanData(finaldf):
                 rows_to_drop.append(i)
                 continue
 
-            finaldf.iloc[i, :] = finaldf.iloc[i, :] #.astype('float')
+            finaldf.iloc[i, :] = finaldf.iloc[i, :] #.astype(np.float32)
         except ValueError:
             rows_to_drop.append(i)
 
@@ -60,107 +61,30 @@ def cleanData(finaldf):
 
 
 df = cleanData(finaldf)
-# finaldf['category'].astype('int')
-print(df.head())
-temporary = finaldf.iloc[:,:5]
-print(temporary.head())
-print(temporary.info())
-print(temporary.dtypes)
+df['category'].astype(np.float32)
 
 print("\n -----Building the Neural Network ------- ")
-df = np.array(df)
+df = np.array(df,dtype=np.float32)
 m, n = df.shape
 X = df[:,1:]
 Y = df[:,0]
+temporary = finaldf.iloc[:,:5]
+print(temporary.head())
+print(temporary.info())
 
-x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.1, random_state=42, shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2, random_state=42, shuffle=True)
 print(x_train.shape), print(y_train.shape), print(x_test.shape), print(y_test.shape)
-
-x_train = x_train.T  # shape - 100,1870
-x_test = x_test.T  # shape - 100,208
-
-# initialize parameters
-def init_param():
-    W1 = np.random.rand(3,x_train.shape[0]) - 0.5
-    b1 = np.random.rand(3,1) - 0.5
-    W2 = np.random.rand(3,3) - 0.5
-    b2 = np.random.rand(3,1) - 0.5
-    return W1, b1, W2, b2
-
-def ReLU(Z):
-    return np.maximum(0,Z)
-
-def derivReLU(Z):
-    np.where(Z > 0, 1, 0)
-
-def softmax(Z):
-    Z = np.array(Z, dtype=np.float64)  # Convert to numpy array
-    Z_exp = np.exp(Z - np.max(Z))
-    return Z_exp / np.sum(Z_exp, axis=0, keepdims=True)
-
-def forward_propagation(W1, b1, W2, b2, X):
-    Z1 = np.dot(W1,X) + b1
-    A1 = ReLU(Z1)
-    Z2 = np.dot(W2,A1) + b2
-    A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
-
-def one_hot(Y):
-    num_classes = int(np.max(Y)) + 1
-    one_hot_Y = np.eye(num_classes, dtype=int)[Y.astype(int)].T
-    return one_hot_Y
-
-
-def back_propagation(Z1, A1, Z2, A2, W2, X, Y):
-    m = Y.size
-    one_hot_Y = one_hot(Y)
-    dZ2 = A2 - one_hot_Y
-    dW2 = 1/m * np.dot(dZ2, A1.T)
-    db2 = 1/m * np.sum(dZ2, axis=1, keepdims=True)
-    dZ1 = np.dot(W2.T, dZ2) * derivReLU(Z1)
-    dW1 = 1/m * np.dot(dZ1, X.T)
-    db1 = 1/m * np.sum(dZ1, axis=1, keepdims=True)
-    return dW1, db1, dW2, db2
-
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-    W1 -= alpha * dW1
-    b1 -= alpha * db1
-    W2 -= alpha * dW2
-    b2 -= alpha * db2
-    return W1, b1, W2, b2
-
-def get_accuracy(predictions, Y):
-    return np.sum(predictions == Y) / Y.size
-
-def get_predictions(A2):
-    return np.argmax(A2, axis=0)
-
-def gradient_descent(X, Y, iterations, alpha):
-    W1, b1, W2, b2 = init_param()
-    for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_propagation(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = back_propagation(Z1, A1, Z2, A2, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-
-        if i % 50 == 0:
-            print("iteration: ",i)
-            print("accuracy: ", get_accuracy(get_predictions(A2), Y))
-    return W1, b1, W2, b2
-
-
-#W1, b1, W2, b2 = gradient_descent(x_train, y_train, 500, 0.1)
-
-x_train = x_train.T
-x_test = x_test.T
 
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.fit_transform(x_test)
-
+kf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
 # Creating and training the MLP classifier
-mlp = MLPClassifier(hidden_layer_sizes=(100, 100), activation='relu', solver='adam', random_state=42)
-mlp.fit(x_train, y_train)
+mlp = MLPClassifier(hidden_layer_sizes=(60, 60), activation='tanh', solver='adam',
+                    learning_rate_init=0.001, random_state=42,validation_fraction=0.2)
 
+scores = cross_val_score(mlp, x_train, y_train, cv=kf)
+mlp.fit(x_train, y_train)
 # Making predictions on the test set
 y_pred = mlp.predict(x_test)
 
