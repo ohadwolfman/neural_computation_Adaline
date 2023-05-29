7b33999c821d371e5347aa3f44c9e9ad1f706561
Ohad Wolfman <98156296+ohadwolfman@users.noreply.github.com>
2023-05-29 16:29:45 +0300

    Add files via upload

--- back_propagation_NN.py
+++ back_propagation_NN.py
@@ -8,12 +8,14 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
+
 print(" -----loading the files------- ")
 # from the folder 'קבצים (מ,ב,ל)' we generated united file:
 file1 = 'Data_regular_writing.txt'
 
 # from the folder 'קבצים בהטייה של 15_ לשני הצדדים (מ,ב,ל)' we generated united file:
 file2 = 'Data_15%_rotated_writing.txt'
+
 
 # Convert the file's content into DataFrame
 def read_and_create_df(fila_name):
@@ -27,17 +29,21 @@ def read_and_create_df(fila_name):
     return df
 
 
-# Join the DataFrames from both of the files
-finaldf = pd.concat([read_and_create_df(file1),
-                    read_and_create_df(file2)], ignore_index=True)
+def concatFilesToDf(file1, file2):
+    # Join the DataFrames from both of the files
+    return pd.concat([read_and_create_df(file1),
+                      read_and_create_df(file2)], ignore_index=True)
+
+
+finaldf = concatFilesToDf(file1, file2)
 
 # Changing the first column to 'category' - that present the label column
-finaldf.rename({0:'category'}, axis=1, inplace=True)
-
+finaldf.rename({0: 'category'}, axis=1, inplace=True)
 
 print("\n -----DataFrame details ------- ")
-print("First dataFrame has",finaldf.shape[0],"rows of", finaldf.shape[1],"pixels")
+print("First dataFrame has", finaldf.shape[0], "rows of", finaldf.shape[1], "pixels")
 print(finaldf.head())
+
 
 print("\n -----Clean the DataFrame------- ")
 def cleanData(finaldf):
@@ -50,7 +56,7 @@ def cleanData(finaldf):
                 rows_to_drop.append(i)
                 continue
 
-            finaldf.iloc[i, :] = finaldf.iloc[i, :] #.astype(np.float32)
+            finaldf.iloc[i, :] = finaldf.iloc[i, :]  # .astype(np.float32)
         except ValueError:
             rows_to_drop.append(i)
 
@@ -63,31 +69,61 @@ def cleanData(finaldf):
 df = cleanData(finaldf)
 df['category'].astype(np.float32)
 
+
+print("\n -----The data contains 3 types of Hebrew letters: ב,ל,מ ------- ")
+print(" -----Creating 3 tables to Comparisons: (ב,ל),(ב,מ),(ל,מ) ------- ")
+condition1 = df['category'] == '1'  # 1 represent the letter ב
+condition2 = df['category'] == '2'  # 2 represent the letter ל
+condition3 = df['category'] == '3'  # 3 represent the letter מ
+data23 = df[~condition1]
+data13 = df[~condition2]
+data12 = df[~condition3]
+data23 = data23.reset_index().drop('index', axis=1)
+data13 = data13.reset_index().drop('index', axis=1)
+data12 = data12.reset_index().drop('index', axis=1)
+
+print(f"Shape of data12: {data12.shape}")
+print(f"Shape of data23: {data23.shape}")
+print(f"Shape of data13: {data13.shape}")
+
+
 print("\n -----Building the Neural Network ------- ")
-df = np.array(df,dtype=np.float32)
-m, n = df.shape
-X = df[:,1:]
-Y = df[:,0]
-temporary = finaldf.iloc[:,:5]
-print(temporary.head())
-print(temporary.info())
+def buildNeuralNetwork(df):
 
-x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2, random_state=42, shuffle=True)
-print(x_train.shape), print(y_train.shape), print(x_test.shape), print(y_test.shape)
+    if (df['category'] == '3').any() and (df['category'] == '2').any():
+        print("----------Comparing Lamed vs Mem----------\n")
+    elif (df['category'] == '1').any() and (df['category'] == '2').any():
+        print("----------Comparing Lamed vs Bet----------\n")
+    elif (df['category'] == '3').any() and (df['category'] == '1').any():
+        print("----------Comparing Bet vs Mem----------\n")
 
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.fit_transform(x_test)
-kf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-# Creating and training the MLP classifier
-mlp = MLPClassifier(hidden_layer_sizes=(60, 60), activation='tanh', solver='adam',
-                    learning_rate_init=0.001, random_state=42,validation_fraction=0.2)
+    # df = np.array(df, dtype=np.float32)
+    X = df.iloc[:, 1:]
+    Y = df.iloc[:, 0]
+    temporary = finaldf.iloc[:, :5]
+    print(temporary.head())
 
-scores = cross_val_score(mlp, x_train, y_train, cv=kf)
-mlp.fit(x_train, y_train)
-# Making predictions on the test set
-y_pred = mlp.predict(x_test)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42, shuffle=True)
+    # print(x_train.shape), print(y_train.shape), print(x_test.shape), print(y_test.shape)
 
-# Evaluating the accuracy of the classifier
-accuracy = np.mean(y_pred == y_test)
-print("Accuracy:", accuracy)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.fit_transform(x_test)
+    kf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    # Creating and training the MLP classifier
+    mlp = MLPClassifier(hidden_layer_sizes=(100, 70, 100), activation='relu', solver='adam',
+                        learning_rate_init=0.001, random_state=42, validation_fraction=0.2)
+
+    scores = cross_val_score(mlp, x_train, y_train, cv=kf)
+    mlp.fit(x_train, y_train)
+    # Making predictions on the test set
+    y_pred = mlp.predict(x_test)
+
+    # Evaluating the accuracy of the classifier
+    accuracy = np.mean(y_pred == y_test)
+    print("Accuracy:", accuracy)
+
+
+buildNeuralNetwork(data12)
+buildNeuralNetwork(data13)
+buildNeuralNetwork(data23)
